895dc48650d57d0881c3f9db39a96e5381c800f3
Alazne <Alazne@LAPTOP-1T5K5LEE>
2025-01-21 21:43:54 +0100

    clases creadas

---
+++ Actividad_16_ii/src/main/java/org/zabalburu/daw1/actividad_16_ii/Actividad_16_ii.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.zabalburu.daw1.actividad_16_ii;
+
+/**
+ *
+ * @author Alazne
+ */
+public class Actividad_16_ii {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
